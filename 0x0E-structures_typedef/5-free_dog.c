0179561c09ff5f2116417a1d40294926604d9305
Jesicakeyo <Jessica.akeyo@gmail.com>
2022-04-11 06:28:21 -0700

    free

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,12 +9,10 @@
 
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		if (d->name)
-			free(d->name);
-		if (d->owner)
-			free(d->owner);
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }
